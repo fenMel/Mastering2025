0d7e6535a57e8880b69632a196de843da1ed211f
Prisca KOUENIN <priscakebi00@gmail.com>
2025-04-14 15:41:35 +0200

    new sessions de formation

--- src/main/java/fr/esic/mastering/repository/SessionFormationRepository.java
+++ src/main/java/fr/esic/mastering/repository/SessionFormationRepository.java
@@ -2,12 +2,20 @@ package fr.esic.mastering.repository;
 
 
 import fr.esic.mastering.entities.SessionFormation;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository  // Ajoute cette annotation si elle n'est pas présente
 public interface SessionFormationRepository extends JpaRepository<SessionFormation, Long> {
+
+    Optional<SessionFormation> findByTitre(String titre);
     // Tu peux ajouter des méthodes spécifiques ici si nécessaire 
+
+
+    
 }
 
 
