7f5453c0647dd1a873429fe3eae2fb8ac371f503
fenMel <femelissa@etudiant-esic.fr>
2025-04-24 14:27:59 +0200

    autoriser tout les port

--- src/main/java/fr/esic/mastering/security/SecurityConfig.java
+++ src/main/java/fr/esic/mastering/security/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // URL de votre frontend Angular
+        configuration.addAllowedOriginPattern("*"); // Autoriser toutes les origines
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
